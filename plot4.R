7e8a7d741003396960650d57f6aa96fe6b885d81
macc0der <shanthan.kesharaju@gmail.com>
2017-01-08 23:23:20 -0500

    minor modification to plot4

--- plot4.R
+++ plot4.R
@@ -12,6 +12,8 @@ jan_and_feb_data <- subset(data, Date>=fromDate & Date<=toDate)
 
 datetime <- paste(as.character(jan_and_feb_data$Date),as.character(jan_and_feb_data$Time))
 
+##Create a png image of the graphs
+png(filename = "plot4.png",width = 480, height=480)
 par(mfrow=c(2,2))
 
 ##Create a line graph of datetime and Global Active Power
@@ -27,13 +29,10 @@ with(jan_and_feb_data, plot(ymd_hms(datetime),Sub_metering_1, type="n", xlab = "
 lines(ymd_hms(datetime),jan_and_feb_data$Sub_metering_1)
 lines(ymd_hms(datetime),jan_and_feb_data$Sub_metering_2, col="red")
 lines(ymd_hms(datetime),jan_and_feb_data$Sub_metering_3, col="blue")
-legend("topright",legend=c("Sub_meetering_1","Sub_meetering_2","Sub_meetering_3"), col=c("black","red","blue"),lty=c(1,1,1))
+legend("topright",legend=c("Sub_meetering_1","Sub_meetering_2","Sub_meetering_3"), col=c("black","red","blue"),lty=c(1,1,1),bty="n")
 
 ##Create a line graph of datetime and Global Reactive Power
 with(jan_and_feb_data, plot(ymd_hms(datetime),Global_reactive_power, type="n", xlab = "datetime", ylab = "Global_reactive_power"))
 lines(ymd_hms(datetime),jan_and_feb_data$Global_reactive_power)
-
-##Copy the graph from the screen device to a png file
-dev.copy(png,file="plot4.png", width=480, height=480)
 
 dev.off()
